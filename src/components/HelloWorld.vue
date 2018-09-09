33eeb616fb9f1678cf2d68db19bccaf8681d3f34
Nguyen Hieu <nguyenhuuthihieu@gmail.com>
2018-09-09 10:38:25 +0700

    vue basic

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,21 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div>
+    {{msg}}
+    <div id="app-4">
+      <ol>
+        <li v-for="todo in todos" :key="todo.id">
+          {{ todo.text }}
+        </li>
+      </ol>
+    </div>
+    <div>
+      <p>{{ message }}</p>
+      <input v-model="message">
+    </div>
+    <div>
+      <p>{{ message }}</p>
+      <button v-on:click="reverseMessage">Reverse Message</button>
+    </div>
   </div>
 </template>
 
@@ -88,7 +24,18 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      todos: [
+        { text: 'Học JavaScript' },
+        { text: 'Học Vue' },
+        { text: 'Xây dựng cái gì đó hay ho' }
+      ],
+      message: 'Hãy sửa thông điệp này'
+    }
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split(' ').reverse().join(' ')
     }
   }
 }
